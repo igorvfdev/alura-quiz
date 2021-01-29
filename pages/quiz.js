db9c185bd688a28fb7d763da9459c1accd57d6ce
igorvfdev <igor_vf@outlook.com>
2021-01-29 00:30:27 -0300

    update quiz page

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,16 +1,50 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import QuizScreen from '../../src/screens/Quiz';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+import Button from '../src/components/Button';
+import Widget from '../src/components/Widget';
 import db from '../db.json';
+import GitHubCorner from '../src/components/GitHubCorner'
+
 
 export default function QuizDaGaleraPage() {
-  return (
-    <ThemeProvider theme={db.theme}>
-      <QuizScreen
-        externalQuestions={db.questions}
-        externalBg={db.bg}
-      />
-    </ThemeProvider>
-  );
+  return ( 
+  <QuizBackground backgroundImage={db.bg}>
+    <QuizContainer>
+      <QuizLogo />
+      <Widget>
+        <Widget.Header>
+          <h3>
+              Pergunta
+              1
+              de
+              {` ${db.questions.lenght}`}
+          </h3>
+        </Widget.Header>
+        <img
+            alt="Descrição"
+            style={{
+                width: '100%',
+                height: '150px',
+                objectFit: 'cover',
+            }}
+                src="https://placehold.it/400x400"
+            />
+            <Widget.Content>
+                <h2>
+                    Titulo
+                </h2>
+                <p>
+                    Descrição
+                </p>
+            <Button>
+                Confirmar           
+            </Button>
+        </Widget.Content>
+      </Widget>
+    </QuizContainer>
+    <GitHubCorner />
+  </QuizBackground>
+);
 }
