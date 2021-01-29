f31ba6283981406885035c2752ec92725e21aa7f
igorvfdev <igor_vf@outlook.com>
2021-01-29 00:02:30 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,15 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 
 import db from '../db.json';
+import QuizLogo from '../src/components/QuizLogo';
 import Widget from '../src/components/Widget/index';
 import QuizBackground from '../src/components/QuizBackground';
 import Footer from '../src/components/Footer/index.js';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
+import QuizContainer from '../src/components/QuizContainer';
 
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 export default function Home() {
   const router = useRouter();
@@ -29,6 +23,7 @@ export default function Home() {
         <title>AluraQuiz</title>
       </Head>
       <QuizContainer>
+        <QuizLogo />
         <Widget>
           <Widget.Header>
             <h1>Titulo Principal</h1>
@@ -40,16 +35,15 @@ export default function Home() {
               router.push(`/quiz?name=${name}`);
               console.log('Fazendo submissao por meio do react');
             }}>
-              <input 
-              onChange = {function (e){
-                console.log(e.target.value);
-                setName(e.target.value)
-              }}
-              placeholder="Diz seu nomne" />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar 
-                {name}
-              </button>
+              <Input
+                name = "nomeDoUsuario"
+                onChange = {(e) => setName(e.target.value)}
+                placeholder="Diz seu nome"
+                value = {name}
+              />
+              <Button type="submit" disabled={name.length === 0}>
+                {`Jogar ${name}`}            
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,9 +1,16 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import QuizScreen from '../../src/screens/Quiz';
+import db from '../db.json';
 
-export default function QuizPage(){
-    return(
-        <div>
-            Pagina Quiz
-        </div>
-    )
+export default function QuizDaGaleraPage() {
+  return (
+    <ThemeProvider theme={db.theme}>
+      <QuizScreen
+        externalQuestions={db.questions}
+        externalBg={db.bg}
+      />
+    </ThemeProvider>
+  );
 }
