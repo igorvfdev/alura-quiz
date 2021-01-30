c7ab5489e5ee1dfd56adc6d524fe399ef75aeab2
igorvfdev <igor_vf@outlook.com>
2021-01-29 23:05:40 -0300

    page quiz

--- pages/quiz.js
+++ pages/quiz.js
@@ -1,50 +1,149 @@
 /* eslint-disable react/prop-types */
+import React from 'react';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizContainer from '../src/components/QuizContainer';
 import Button from '../src/components/Button';
 import Widget from '../src/components/Widget';
 import db from '../db.json';
-import GitHubCorner from '../src/components/GitHubCorner'
+import GitHubCorner from '../src/components/GitHubCorner';
 
+function LoadingWidget() {
+  return (
+    <Widget>
+      <Widget.Header>
+        Carregando...
+      </Widget.Header>
 
-export default function QuizDaGaleraPage() {
-  return ( 
-  <QuizBackground backgroundImage={db.bg}>
-    <QuizContainer>
-      <QuizLogo />
-      <Widget>
-        <Widget.Header>
-          <h3>
-              Pergunta
-              1
-              de
-              {` ${db.questions.length}`}
-          </h3>
-        </Widget.Header>
-        <img
-            alt="Descrição"
-            style={{
-                width: '100%',
-                height: '150px',
-                objectFit: 'cover',
-            }}
-                src="https://placehold.it/400x400"
-            />
-            <Widget.Content>
-                <h2>
-                    Titulo
-                </h2>
-                <p>
-                    Descrição
-                </p>
-            <Button>
-                Confirmar           
-            </Button>
-        </Widget.Content>
-      </Widget>
-    </QuizContainer>
-    <GitHubCorner />
-  </QuizBackground>
-);
+      <Widget.Content>
+        [Desafio do Loading]
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+function QuestionWidget({
+  question,
+  totalQuestions,
+  questionIndex,
+  onSubmit,
+}) {
+  const questionId = `question__${questionIndex}`;
+  return (
+    <Widget>
+      <Widget.Header>
+        <h3>
+          {`Pergunta ${questionIndex + 1} de ${db.questions.length}`}
+        </h3>
+      </Widget.Header>
+      <img
+        alt="Descrição"
+        style={{
+          width: '100%',
+          height: '150px',
+          objectFit: 'cover',
+        }}
+        src={question.image}
+      />
+      <Widget.Content>
+        <h2>
+          {question.title}
+        </h2>
+        <p>
+          {question.description}
+        </p>
+
+        <form onSubmit={(e) => {
+          e.preventDefault();
+          onSubmit();
+        }}
+        >
+
+          {question.alternatives.map((alternative, alternativeIndex) => {
+            const alternativeId = `alternative__${alternativeIndex}`;
+            return (
+              <Widget.Topic
+                htmlFor={alternativeId}
+              >
+                <input
+                  //style={{ display: 'none' }}
+                  id={alternativeId}
+                  name={questionId}
+                  type="radio"
+                />
+                {alternative}
+              </Widget.Topic>
+            );
+          })}
+
+          {/* <pre>
+            {JSON.stringify(question, null, 4)}
+          </pre> */}
+
+          <Button type="submit">
+            Confirmar
+          </Button>
+        </form>
+      </Widget.Content>
+    </Widget>
+  );
+}
+
+const screenStates = {
+  QUIZ: 'QUIZ',
+  LOADING: 'LOADING',
+  RESULT: 'RESULT',
+};
+
+export default function QuizPage() {
+  const [screenState, setScreenState] = React.useState(screenStates.LOADING);
+  const totalQuestions = db.questions.length;
+  const [currentQuestion, setCurrentQuestion] = React.useState(0);
+  const questionIndex = currentQuestion;
+  const question = db.questions[questionIndex];
+
+  // [React chama de: Efeitos || Effects]
+  // React.useEffect
+  // atualizado === willUpdate
+  // morre === willUnmount
+  React.useEffect(() => {
+    setTimeout(() => {
+      setScreenState(screenStates.QUIZ);
+    }, 1 * 1000);
+    // nasce === didMount
+  }, []);
+
+  function handleSubmit() {
+    setCurrentQuestion(questionIndex + 1);
+  }
+
+  function handleSubmitQuiz() {
+    const nextQuestion = questionIndex + 1;
+    if (nextQuestion < totalQuestions) {
+      setCurrentQuestion(questionIndex + 1);
+    } else {
+      setScreenState(screenStates.RESULT);
+    }
+  }
+
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <QuizLogo />
+
+        {screenState === screenStates.QUIZ && (
+          <QuestionWidget
+            question={question}
+            questionIndex={questionIndex}
+            totalQuestions={totalQuestions}
+            onSubmit={handleSubmitQuiz}
+          />
+        )}
+        {screenState === screenStates.LOADING && <LoadingWidget />}
+
+        {screenState === screenStates.RESULT && <div>Você acertou X questôes, parabéns!</div>}
+      </QuizContainer>
+      <GitHubCorner />
+    </QuizBackground>
+  );
 }
